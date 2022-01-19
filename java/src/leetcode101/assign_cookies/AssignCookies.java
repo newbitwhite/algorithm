92756d420d0b744a8ab9d1d7af20a74a225b7ad4
xiaobai <1107671507@qq.com>
2022-01-19 17:59:35 +0800

    feat(leetcode): candy
    add comments

--- java/src/leetcode101/assign_cookies/AssignCookies.java
+++ java/src/leetcode101/assign_cookies/AssignCookies.java
@@ -2,18 +2,6 @@ package leetcode101.assign_cookies;
 
 import java.util.Arrays;
 
-/**
- *题目描述: 有一群孩子和一堆饼干，每个孩子有一个饥饿度，每个饼干都有一个大小。每个孩子只能吃
- *最多一个饼干，且只有饼干的大小大于等于孩子的饥饿度时，这个孩子才能吃饱。求解最多有多少孩
- *可以吃饱。
- * <p>
- *输入输出样例: 输入两个数组，分别代表孩子的饥饿度和饼干的大小。输出最多有多少孩子可以吃饱的数
- *量。
- * <p>Input: [1,2], [1,2,3]
- * <p>Output: 2
- * @author ：xiaobai
- * @date ：2022/1/19 14:11
- */
 public class AssignCookies {
     public int assign(int[] kids, int[] cookies){
         Arrays.sort(kids);
--- java/src/leetcode101/candy/Candy.java
+++ java/src/leetcode101/candy/Candy.java
@@ -3,6 +3,7 @@ package leetcode101.candy;
 import java.util.Arrays;
 
 /**
+ * 135.Candy[Hard]
  * @author ：xiaobai
  * @date ：2022/1/19 16:46
  */
@@ -20,7 +21,7 @@ public class Candy {
 
         for (int i = kidsScores.length-1; i > 0; i--) {
             if (kidsScores[i-1] > kidsScores[i]){
-                //易错点：这里的max很容易想不到，漏了
+                //notice：The "max" function may be ignored
                 candies[i-1] = Math.max(candies[i-1], candies[i] + 1);
             }
         }
