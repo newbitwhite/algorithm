795131bb40ebaf7fd7c1656042d8b649ea5f0766
xiaobai <1107671507@qq.com>
2022-01-21 15:12:45 +0800

    feat(leetcode): 406. Queue Reconstruction by Height greedy algorithm

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Greedy
 |455|[Assign Cookies](https://leetcode-cn.com/problems/assign-cookies/)|[leetcode101](./java/src/leetcode101/greedy/assign_cookies)|Easy|
 |605|[Can Place Flowers](https://leetcode-cn.com/problems/can-place-flowers/)|[leetcode101](./java/src/leetcode101/greedy/can_place_flowers)|Easy|
 |763|[Partition Labels](https://leetcode-cn.com/problems/partition-labels/)|[leetcode101](./java/src/leetcode101/greedy/partition_labels)|Medium|
+|122|[Best Time to Buy and Sell Stock II](https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/)|[leetcode101](./java/src/leetcode101/greedy/best_time_to_buy_and_sell_stock_2)|Medium|
+|406|[Queue Reconstruction by Height](https://leetcode-cn.com/problems/queue-reconstruction-by-height/)|[leetcode101](./java/src/leetcode101/greedy/queue_reconstruction_by_height)|Medium|
---
+++ java/src/leetcode101/greedy/queue_reconstruction_by_height/QueueReconstructionByHeight.java
@@ -0,0 +1,30 @@
+package leetcode101.greedy.queue_reconstruction_by_height;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * 406. Queue Reconstruction by Height
+ * @author ：xiaobai
+ * @date ：2022/1/21 14:05
+ */
+public class QueueReconstructionByHeight {
+
+    public int[][] reconstructQueue(int[][] people) {
+        //sort array people:element[1] min to max, if element[1] eq , element[0] max to min
+        Arrays.sort(people, (o1, o2) -> {
+            int compare = Integer.compare(o2[0], o1[0]);
+            if (compare == 0){
+                return Integer.compare(o1[1], o2[1]);
+            }
+            return compare;
+        });
+
+        LinkedList<int[]> list = new LinkedList<>();
+        //two layers of circulation:insert element into reconstructQueue
+        for (int[] person : people) {
+            list.add(person[1], person);
+        }
+        return list.toArray(new int[people.length][]);
+    }
+}
