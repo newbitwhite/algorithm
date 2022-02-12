e486ae79d33438314fe257c996ee7139da3a286a
xiaobai <1107671507@qq.com>
2022-02-12 09:54:51 +0800

    feat(leetcode101:binary search): 4. Median of Two Sorted Arrays [Hard] binary search

--- JavaVersion/src/main/java/leetcode101/binary_search/median_of_two_sorted_arrays/MedianOfTwoSortedArrays.java
+++ JavaVersion/src/main/java/leetcode101/binary_search/median_of_two_sorted_arrays/MedianOfTwoSortedArrays.java
@@ -27,19 +27,44 @@ public class MedianOfTwoSortedArrays {
     }
 
     private int getKthElement(int[] nums1, int[] nums2, int k) {
-        int nums1Pos = 0;
-        int nums2Pos = 0;
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        while (k > 1) {
-            int index = k / 2 - 1;
-            if (nums1[(Math.min(len1 - 1, nums1Pos + index))] >= nums2[(Math.min(len2 - 1, nums2Pos + index))]) {
-                nums2Pos += index + 1;
-            } else {
-                nums1Pos += index + 1;
+        /* 主要思路：要找到第 k (k>1) 小的元素，那么就取 pivot1 = nums1[k/2-1] 和 pivot2 = nums2[k/2-1] 进行比较
+         * 这里的 "/" 表示整除
+         * nums1 中小于等于 pivot1 的元素有 nums1[0 .. k/2-2] 共计 k/2-1 个
+         * nums2 中小于等于 pivot2 的元素有 nums2[0 .. k/2-2] 共计 k/2-1 个
+         * 取 pivot = min(pivot1, pivot2)，两个数组中小于等于 pivot 的元素共计不会超过 (k/2-1) + (k/2-1) <= k-2 个
+         * 这样 pivot 本身最大也只能是第 k-1 小的元素
+         * 如果 pivot = pivot1，那么 nums1[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums1 数组
+         * 如果 pivot = pivot2，那么 nums2[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums2 数组
+         * 由于我们 "删除" 了一些元素（这些元素都比第 k 小的元素要小），因此需要修改 k 的值，减去删除的数的个数
+         */
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+        int index1 = 0;
+        int index2 = 0;
+
+        while (true) {
+            // 边界情况
+            if (index1 == length1) {
+                return nums2[index2 + k - 1];
             }
-            k -= (index + 1);
+            if (index2 == length2) {
+                return nums1[index1 + k - 1];
+            }
+            if (k == 1) {
+                return Math.min(nums1[index1], nums2[index2]);
+            }
+
+            // 正常情况
+            int half = k / 2;
+            int newIndex1 = Math.min(index1 + half, length1) - 1;
+            int newIndex2 = Math.min(index2 + half, length2) - 1;
+            if (nums1[newIndex1] <= nums2[newIndex2]) {
+                k -= (newIndex1 - index1 + 1);
+                index1 = newIndex1 + 1;
+            } else {
+                k -= (newIndex2 - index2 + 1);
+                index2 = newIndex2 + 1;
+            }
         }
-        return Math.min(nums1[nums1Pos], nums2[nums2Pos]);
     }
 }
