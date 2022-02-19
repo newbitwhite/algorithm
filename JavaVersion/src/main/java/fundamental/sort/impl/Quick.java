bb550a6e98d57f610c6614e9e028e437cb868c0f
xiaobai <1107671507@qq.com>
2022-02-19 08:09:17 +0800

    feat(project): update dir

--- JavaVersion/src/main/java/fundamental/sort/impl/Quick.java
+++ JavaVersion/src/main/java/fundamental/sort/impl/Quick.java
@@ -8,7 +8,19 @@ package fundamental.sort.impl;
  */
 @SuppressWarnings("all")
 public class Quick{
-
     public static void sort(Comparable[] arr) {
+
+        quickSort(arr, 0, arr.length-1);
+    }
+
+    private static void quickSort(Comparable[] arr, int low, int high){
+        Comparable posElem = arr[low];
+        int index = partition(arr, low, high);
+        quickSort(arr, low, index-1);
+        quickSort(arr, index+1, high);
+    }
+
+    private static int partition(Comparable[] arr, int low, int high){
+        return 0;
     }
 }
